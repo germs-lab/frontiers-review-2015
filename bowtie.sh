8c8a7cf18bf2daa307f5c7acf5a90259f54de38e
Ubuntu <ubuntu@ip-10-181-99-24.ec2.internal>
2015-04-18 23:09:32 +0000

    updates

--- bowtie.sh
+++ bowtie.sh
@@ -4,14 +4,16 @@ READS=$2
 echo "Reference is" $REF 
 echo "Reads are" $READS
 
-#bowtie2-2.2.5/bowtie2-build $REF $REF-bowtie-index
-bowtie2-2.2.5/bowtie2 -x $REF-bowtie-index -U $READS -S $REF.$READS.sam
+/home/ubuntu/notebooks/bowtie2-2.2.5/bowtie2-build $REF $REF-bowtie-index
+/home/ubuntu/notebooks/bowtie2-2.2.5/bowtie2 -x $REF-bowtie-index -U $READS -S $REF.$READS.sam
 samtools view -Sb $REF.$READS.sam > $REF.$READS.bam
 #https://broadinstitute.github.io/picard/explain-flags.htmls
 samtools view -c -f 4 $REF.$READS.bam > reads-unmapped.count.txt
 samtools view -c -F 4 $REF.$READS.bam > reads-mapped.count.txt
 samtools sort $REF.$READS.bam $REF.$READS.bam.sorted
+
 #SRR172903.NC005008.bam.sorted.bam
 samtools index $REF.$READS.bam.sorted.bam
 samtools idxstats $REF.$READS.bam.sorted.bam > reads.by.contigs.txt
+
 #Retrieve and print stats in the index file. The output is TAB delimited with each line consisting of reference sequence name, sequence length, # mapped reads and # unmapped reads.
---
+++ khmer-install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/ged-lab/khmer.git
+cd khmer
+python setup.py install
+
