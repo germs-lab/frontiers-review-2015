9a4d1c99bb3b48d16b1db5141ee3bb74181a8f38
Adina Howe <adina@Adinas-MacBook-Air.local>
2015-04-20 16:28:30 -0500

    updates

--- download.sh
+++
@@ -1,6 +0,0 @@
-#downloading software
-wget 'http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.5/bowtie2-2.2.5-source.zip'
-unzip bowtie2-2.2.5-source.zip
-cd bowtie2-2.2.5-source
-make all
-#install samtools
