847878ba916c12dd592c91f39cd7b077c0b2ec69
root <root@ip-10-69-66-161.ec2.internal>
2015-06-26 14:00:34 +0000

    updated install fastx

--- fastx_install.sh
+++ fastx_install.sh
@@ -1,10 +1,11 @@
+sudo apt-get -y install gcc g++ pkg-config
 cd libgtextutils-0.7
-./configure
-make
+sudo ./configure
+sudo make
 sudo make install
 cd ..
 cd fastx_toolkit-0.0.14
-./configure
+sudo ./configure
 sudo make 
 sudo make install
 cd ..
